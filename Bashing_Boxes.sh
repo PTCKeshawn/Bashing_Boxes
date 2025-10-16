9ea2a0f9d8c3543599ed7dc8d4da250a9732b4cd
PTCKeshawn <keshawnchidionor@gmail.com>
2025-10-16 15:28:23 -0500

    added some lines to the script

---
+++ Bashing_Boxes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+random_array=(
+	"cookie jar" 
+	"sweater" 
+	"sword" 
+	"curtain rod" 
+	"tree stump" 
+	"parasol" 
+	"mittens" 
+	"hockey puck" 
+	"beet" 
+	"stroller"
+)
+close=1
+
+echo "What would you like to do with the array?"
+sleep 1
+echo "1. print all the words"
+echo "2. print a word of your liking"
+echo "1. add an element to the array"
+echo "1. delete an element from the array"
+
+while [$close -eq 1]; do
+	if []
+
+
+Print_array() {
+	echo ${random_array[@]}
+}
