99961ffd6cc911b4228591ff48f29d666a8f2a2f
PTCKeshawn <keshawnchidionor@gmail.com>
2025-10-17 15:26:47 -0500

    added more functions for the option

--- Bashing_Boxes.sh
+++ Bashing_Boxes.sh
@@ -19,6 +19,32 @@ Print_array() {
 	sleep 1
 
 }
+print_single_word(){
+	
+	echo "enter the index of the object you want to print."
+	read -p "hint: The first element on the list is counted as 0." index
+#this line means if the user put a number that is 0 or higher, and smaller than the number of items in the array, then do the following
+	if [ "$index" -ge 0 ] && [ "$index" -lt "${#random_array[@]}" ]; then
+		echo "You chose: ${random_array[$index]}"
+	else
+		echo "invalid"
+	fi
+sleep 1
+}
+
+add_item(){
+	read -p "what word do you want to add" word
+	read -p "where do you want to add the item in the array?"
+
+	 if [ "$pos" -ge 0 ] && [ "$pos" -le "${#random_array[@]}" ]; then
+        # Splits the array to add the word
+       random_array=("${random_array[@]:0:$pos}" "$word" "${random_array[@]:$pos}")
+        echo "Array after insertion: ${random_array[@]}"
+    else
+        echo "Invalid position."
+    fi
+}
+
 close=1
 
 
@@ -30,12 +56,19 @@ while [ $close -eq 1 ]; do
 	echo "2. print a word of your liking"
 	echo "3. add an element to the array"
 	echo "4. delete an element from the array"
+	echo "5. exit"
 	read -p "what is your choice" choice
 	if [ $choice -eq 1 ]; then
 		Print_array
+	elif [ $choice -eq 2 ]; then
+		print_single_word
+	
+		
+	
 	else
 		echo "invalid"
 		exit
 	fi
+
 done
 
